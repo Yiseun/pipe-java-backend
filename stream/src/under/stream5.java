f97c93038b87b65a1e08246cebede85212fedd10
Seun Lee <sleet99@naver.com>
2022-06-06 21:54:26 +0900

    :recycle: Refactor : Improve structure

--- stream/src/under/stream5.java
+++ stream/src/under/stream5.java
@@ -8,8 +8,9 @@ public class stream5 {
 	public static void main(String[] args) {
 	    List<String> names = Arrays.asList("이산", "진영", "이산", "이산", "별찬");
 	    names.stream()
-	      .distinct()
-	      .forEach(System.out::println);
+	        .distinct() // 중복 제거
+	        .filter(name -> name.equals("이산")) // 이산이라는 이름을 가진 이름만 리스트에 남겨 둠.
+	        .forEach(System.out::println);
 	}
 
 }
